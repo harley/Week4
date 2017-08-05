364918957e9f3d2dbb0773634d80e073b0006dbb
landroide13 <lazaro02@hotmail.com>
2017-08-05 10:19:03 +0700

    saturday

--- practice1/app/controllers/files_controller.rb
+++ practice1/app/controllers/files_controller.rb
@@ -1,9 +1,13 @@
 class FilesController < ApplicationController
 
+
   def index
-    @files = Dir["*"]
+    if params[:next]
+      path = params[:next] + "/*"
+      @files = Dir[path]
+
+      @parent = path.split("/")[0..-3].join("/")
+    else
+      @files = Dir["*"]
   end
-
-
-
 end
--- practice1/config/routes.rb
+++ practice1/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+resource :lists
+
   get 'say/hello'
 
   get 'say/goodbye'
