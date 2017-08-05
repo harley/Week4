13b9524c3bb255c6e5b98d1828e2fc1425a3623c
landroide13 <lazaro02@hotmail.com>
2017-08-05 11:25:58 +0700

    saturday

---
+++ assignment/Resto2/app/controllers/menu_controller.rb
@@ -0,0 +1,5 @@
+class MenuController < ApplicationController
+  def index
+    @category = Category.all
+  end
+end
--- assignment/Resto2/config/routes.rb
+++ assignment/Resto2/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root "home#index"
   
+  get 'menu' => 'menu#index'
   get 'index' => "cart#index"
 
   
