d105adb19f11f6a8afec2e51332a70d2f299b2ff
landroide13 <lazaro02@hotmail.com>
2017-08-08 10:52:38 +0700

    tusday

---
+++ assignment/Resto2/db/migrate/20170808034804_create_line_items.rb
@@ -0,0 +1,11 @@
+class CreateLineItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :line_items do |t|
+      t.references :items, foreign_key: true
+      t.belongs_to :cart, foreign_key: true
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
--- assignment/Resto2/db/seeds.rb
+++ assignment/Resto2/db/seeds.rb
@@ -16,7 +16,7 @@ end
 if Item.count == 0
   Section.all.each do |section|
     4.times do |index|
-      a = Item.create!(name:Faker::Food.dish,description:Faker::Food.ingredient,price:'20,000',section: section)
+      a = Item.create!(name:Faker::Food.dish,description:Faker::Food.ingredient,price:rand(10..200) * 1000,section: section)
       a.image_url = "#{section.name}#{index+1}.jpg"# Breakfast1.jpg
       a.save!
     end
